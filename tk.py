7890551ad2bca1341d7bf1ad54bb5baac07e0bc7
meopkids@gmail.com <meopkids@gmail.com>
2019-08-31 22:23:53 +0800

    can start and stop

--- tk.py
+++ tk.py
@@ -6,28 +6,23 @@ import math
 root = tk.Tk()
 root.geometry('500x300')
 
-var = tk.StringVar()
-var ='press start'
-btn_text = tk.StringVar()
-# lambda create instance of Clock()
-# automatic initalize init time
-start = tk.Button(root ,text='start',
-                    command= lambda : Clock())
-
-label = tk.Label(root, text=var)
-quit = tk.Button(root, text='Quit',command=exit)
-
+conti = True
 class Clock():
-    global btn_text
+    global btn_text, conti
     def __init__(self):
         self.init_time = t.time()
         self.timer()
 
+
     def timer(self):
         recall = Timer(1.0, self.timer)
-        time_elape = t.time() - self.init_time
-        label['text']= self.formater(time_elape)
-        recall.start()
+        if conti:
+            start['text'] = 'stop'
+            time_elape = t.time() - self.init_time
+            label['text']= self.formater(time_elape)
+            recall.start()
+        elif not conti:
+            recall.cancel()
         
     def formater(self,sec):
         _hour = math.floor(sec/3600)
@@ -35,12 +30,25 @@ class Clock():
         _sec = math.floor(sec - (_min*60) - (_hour*3600))
         return '%02d hour %02d min %02d sec' % (_hour,_min,_sec)
 
+def stop1():
+    global conti
+    conti = False
+
 def exit():
     root.destroy()
+
+var = tk.StringVar()
+var ='press start'
+btn_text = tk.StringVar()
+# lambda create instance of Clock()
+# automatic initalize init time
+start = tk.Button(root ,text='start',
+                    command= lambda : Clock())
 start.pack()
+stop_btn = tk.Button(root, text='stop!', command= stop1)
+stop_btn.pack()
+label = tk.Label(root, text=var)
 label.pack()
-
+quit = tk.Button(root, text='Quit',command=exit)
 quit.pack()
-
-
 root.mainloop()
