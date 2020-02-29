f171da010ea27fce5269dcc2fe8377721f31f5c5
ARJhe <meopkids@gmail.com>
2020-02-29 16:33:40 +0800

    1. build executable file by "pyinstaller -F --noconsole --onefile .\clock.py" \n2. add img to btn

--- clock.py
+++ clock.py
@@ -8,16 +8,6 @@ currentTime = 0
 loopRound = 0
 stopFlag = Event()
 
-class Interface(tk.Tk):
-    def __init__(self):
-        super().__init__()    
-
-    def exit(self):
-        return super(Interface, self).destroy()
-
-    def hide_me(self, event):
-        print('hide me')
-        event.widget.place_forget()
 
 class Clock(Thread):    
     def __init__(self, event):
@@ -28,7 +18,8 @@ class Clock(Thread):
         self.state = Condition()        
 
     def start(self, act=None):
-        self.initTime = t.time()
+        self.initTime = t.time()        
+        btnStart.config(state=DISABLED)
         if act:            
             return super(Clock, self).start()      
 
@@ -47,19 +38,20 @@ class Clock(Thread):
 
     def pause(self):
         global currentTime
+        btnResume.config(state=NORMAL)
         currentTime = self.timeElape        
         with self.state:
             self.paused = True  # Block self.
 
     def resume(self):
         self.initTime = t.time() # reset initTime   
+        btnResume.config(state=DISABLED)
         with self.state:
             self.paused = False
             self.state.notify()  # Unblock self if waiting.
 
     def reset(self):
-        global currentTime,loopRound
-        self.pause()
+        global currentTime,loopRound        
         loopRound = 0
         currentTime = 0
         self.resume()
@@ -71,7 +63,7 @@ class Clock(Thread):
         return '%02d : %02d : %02d ' % (_hour,_min,_sec)
 
 
-root = Interface()
+root = tk.Tk()
 root.geometry('400x300')
 root.resizable(0, 0)
 root.config(bg='black')
@@ -80,20 +72,31 @@ labelText ='press start'
 labScreen = tk.Label(root, text=labelText, fg="white", bg="black",height=5)
 labScreen.config(font=("Courier 19 bold"))
 
+
+iconPlay = PhotoImage(file = r"C:\Users\meopk\OneDrive\桌面\python tutuorial\tkinter_tutorial\icons\play.png")
+iconPause = PhotoImage(file = r"C:\Users\meopk\OneDrive\桌面\python tutuorial\tkinter_tutorial\icons\pause.png")
+iconResume = PhotoImage(file = r"C:\Users\meopk\OneDrive\桌面\python tutuorial\tkinter_tutorial\icons\resume.png")
+iconReset = PhotoImage(file = r"C:\Users\meopk\OneDrive\桌面\python tutuorial\tkinter_tutorial\icons\reset.png")
+iconExit = PhotoImage(file = r"C:\Users\meopk\OneDrive\桌面\python tutuorial\tkinter_tutorial\icons\exit.png")
+iconPy = PhotoImage(file = r"C:\Users\meopk\OneDrive\桌面\python tutuorial\tkinter_tutorial\icons\python-logo.png")
 timer = Clock(stopFlag) # Create an instance
-btnStart = tk.Button(root ,text='start',command= lambda : timer.start(True))
-btnStop = tk.Button(root, text='stop', command= timer.pause)
-btnResume = tk.Button(root, text='resume', command= timer.resume)
-btnReset = tk.Button(root, text='reset', command= timer.reset)
-btnExit = tk.Button(root, text='Quit',command=root.exit, bg='white')
+btnStart = tk.Button(root ,text='start',command= lambda : timer.start(True), image = iconPlay)
+btnStop = tk.Button(root, text='stop', command= timer.pause, image = iconPause)
+btnResume = tk.Button(root, text='resume', command= timer.resume, state=DISABLED, image = iconResume)
+btnReset = tk.Button(root, text='reset', command= timer.reset, image = iconReset)
+btnExit = tk.Button(root, text='Exit',command=root.destroy)
+logo = tk.Label(root, image = iconPy)
 
 labScreen.pack(fill='x')
-btnStart.place(relx=0.25,rely=0.4)
-btnStop.place(relx=0.35,rely=0.4)
-btnResume.place(relx=0.45,rely=0.4)
+btnStart.place(relx=0.3,rely=0.4)
+btnStop.place(relx=0.395,rely=0.4)
+btnResume.place(relx=0.5,rely=0.4)
 btnReset.place(relx=0.6,rely=0.4)
-btnExit.place(relx=0.43,rely=0.7)
 
+btnExit.place(relx=0.43,rely=0.7)
+btnExit.pack(ipadx=50, pady=50)
+
+logo.place(relx=0.85,rely=0.8)
 
 if __name__ == "__main__":
     root.mainloop()
